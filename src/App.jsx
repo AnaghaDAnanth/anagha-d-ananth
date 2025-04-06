a652f08b0a031f3b9c3b470955689298b6f86a08
Anagha D Ananth <anagha.d.ananth@sap.com>
2025-04-06 14:36:01 +0530

    Adjust screen size

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Home from './components/Home.jsx'
 
 function App() {
   return (
-    <body  class="absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-      <div style={{transform: "scale(0.9)", transformOrigin: "top left", width: `${100 / 0.9}%`, height: `${100 / 0.9}%`}}>
-        <SocialSidebar/>
-        <Home/>
+    <body style={{margin: 0, overflow: 'hidden'}} className="bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+      <div style={{
+          transform: "scale(0.9)",
+          transformOrigin: "top left",
+          width: `${100 / 0.9}%`,
+          height: `${100 / 0.9}%`,
+        }}>
+        <SocialSidebar />
+        <Home />
       </div>
-    </body>
+  </body>
   )
 }
 
